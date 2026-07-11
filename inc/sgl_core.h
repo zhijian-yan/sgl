487647275c19d5d4d14b7d0af86ed1034c4c6e4d
Zhijian Yan <937279457@qq.com>
2026-07-12 02:11:41 +0800

    refactor: refactoring the buffer slicing function

--- inc/sgl_core.h
+++ inc/sgl_core.h
@@ -64,6 +64,28 @@ typedef enum {
 
 #define SGL_ROTATE_DEFAULT SGL_ROTATE_0
 
+typedef enum {
+    SGL_SLICE_STATUS_IDLE,
+    SGL_SLICE_STATUS_START,
+    SGL_SLICE_STATUS_RUNNIG,
+} sgl_slice_status_t;
+
+typedef enum {
+    SGL_COLOR_FORMAT_MONO,
+    SGL_COLOR_FORMAT_RGB332,
+    SGL_COLOR_FORMAT_RGB565,
+    SGL_COLOR_FORMAT_RGB565SWAP,
+    SGL_COLOR_FORMAT_BGR565,
+    SGL_COLOR_FORMAT_RGB888,
+    SGL_COLOR_FORMAT_BGR888,
+    SGL_COLOR_FORMAT_XRGB8888,
+    SGL_COLOR_FORMAT_XBGR8888,
+    SGL_COLOR_FORMAT_ARGB8888,
+    SGL_COLOR_FORMAT_ABGR8888,
+    SGL_COLOR_FORMAT_RGBA8888,
+    SGL_COLOR_FORMAT_BGRA8888,
+} sgl_color_format_t;
+
 typedef struct {
     int32_t left;
     int32_t top;
@@ -76,17 +98,21 @@ typedef struct sgl_screen sgl_screen_t;
 struct sgl_screen {
     void *buffer;
     uint32_t buffer_size;
-    uint32_t piexl_size;
+    uint32_t pixel_num;
     uint32_t hor_res;
     uint32_t ver_res;
     uint32_t max_x;
     uint32_t max_y;
     uint32_t offset_x;
     uint32_t offset_y;
-    uint32_t fcount;
     sgl_rect_t drawable;
-    sgl_rect_t dirty;
     sgl_rect_t slice;
+    sgl_rect_t dirty;
+    uint32_t w_dirty;
+    uint32_t h_dirty;
+    uint32_t slice_count;
+    uint32_t frame_count;
+    sgl_slice_status_t slice_status;
     sgl_rotate_t rotate;
     void (*draw)(sgl_screen_t *scr);
     void (*flush)(void *buffer, sgl_rect_t *refresh);
@@ -94,21 +120,22 @@ struct sgl_screen {
 };
 
 int sgl_init(sgl_screen_t *scr, void *buffer, uint32_t buffer_size,
-             uint32_t hor_res, uint32_t ver_res);
+             sgl_color_format_t color_format, uint32_t hor_res,
+             uint32_t ver_res);
 void sgl_handler(sgl_screen_t *scr);
-void sgl_set_buffer(sgl_screen_t *scr, void *buffer, uint32_t buffer_size);
 void sgl_set_draw(sgl_screen_t *scr, void (*draw)(sgl_screen_t *scr));
 void sgl_set_flush(sgl_screen_t *scr,
                    void (*flush)(void *buffer, sgl_rect_t *refresh));
-void sgl_set_draw_pixel(sgl_screen_t *scr,
-                        void (*draw_pixel)(sgl_screen_t *scr, int32_t x,
-                                           int32_t y, uint32_t color));
+int sgl_set_draw_pixel(sgl_screen_t *scr,
+                       void (*draw_pixel)(sgl_screen_t *scr, int32_t x,
+                                          int32_t y, uint32_t color),
+                       uint32_t buffer_size, uint32_t pixel_size);
 void sgl_set_drawable(sgl_screen_t *scr, int32_t left, int32_t top,
                       int32_t right, int32_t bottom);
 void sgl_reset_drawable(sgl_screen_t *scr);
 void sgl_set_screen_rotation(sgl_screen_t *scr, sgl_rotate_t rotate);
-uint32_t sgl_get_fcount(sgl_screen_t *scr);
-void sgl_reset_fcount(sgl_screen_t *scr);
+uint32_t sgl_get_frame_count(sgl_screen_t *scr);
+void sgl_reset_frame_count(sgl_screen_t *scr);
 void sgl_clear_screen(sgl_screen_t *scr, uint8_t value);
 
 #ifdef __cplusplus
--- src/sgl_core.c
+++ src/sgl_core.c
@@ -6,39 +6,51 @@
 #include "../inc/sgl_pixel.h"
 #include <string.h>
 
+static void sgl_set_color_format(sgl_screen_t *scr, uint32_t buffer_size,
+                                 sgl_color_format_t color_format);
+
 int sgl_init(sgl_screen_t *scr, void *buffer, uint32_t buffer_size,
-             uint32_t hor_res, uint32_t ver_res) {
+             sgl_color_format_t color_format, uint32_t hor_res,
+             uint32_t ver_res) {
     if (!scr || !buffer)
         return -1;
     memset(scr, 0, sizeof(sgl_screen_t));
     scr->buffer = buffer;
-    scr->buffer_size = buffer_size;
     scr->hor_res = hor_res;
     scr->ver_res = ver_res;
-    sgl_set_draw_pixel(scr, sgl_draw_pixel_mono);
+    sgl_set_color_format(scr, buffer_size, color_format);
     sgl_set_screen_rotation(scr, SGL_ROTATE_DEFAULT);
     scr->dirty = scr->drawable;
     return 0;
 }
 
 static void sgl_buffer_slice(sgl_screen_t *scr) {
-    uint32_t w_slice, h_slice, w_dirty, h_dirty;
-    scr->offset_x = scr->dirty.left;
-    scr->offset_y = scr->dirty.top;
-    h_dirty = scr->dirty.bottom - scr->dirty.top + 1;
-    w_dirty = scr->dirty.right - scr->dirty.left + 1;
-    if (h_dirty == 0 && w_dirty == 0)
-        return;
-    h_slice = scr->piexl_size / w_dirty;
-    w_slice = h_slice ? w_dirty : (scr->piexl_size % w_dirty);
-    if (h_slice == 0)
-        h_slice = 1;
-    if (h_slice > h_dirty)
-        h_slice = h_dirty;
-    sgl_set_rect(&scr->slice, scr->offset_x, scr->offset_y,
-                 scr->offset_x + w_slice - 1, scr->offset_y + h_slice - 1);
-    scr->drawable = scr->slice;
-    scr->dirty.top += h_slice;
+    uint32_t w_cake, h_cake, w_piece, h_piece;
+    switch (scr->slice_status) {
+    case SGL_SLICE_STATUS_IDLE:
+    case SGL_SLICE_STATUS_START:
+        scr->slice_status = SGL_SLICE_STATUS_RUNNIG;
+        scr->slice_count = 0;
+        scr->w_dirty = scr->dirty.right - scr->dirty.left + 1;
+        scr->h_dirty = scr->dirty.bottom - scr->dirty.top + 1;
+    case SGL_SLICE_STATUS_RUNNIG:
+        scr->offset_x = scr->dirty.left;
+        scr->offset_y = scr->dirty.top + scr->slice_count;
+        w_cake = scr->w_dirty;
+        h_cake = scr->h_dirty;
+        w_piece = w_cake;
+        h_piece = scr->pixel_num / w_cake;
+        h_cake -= scr->slice_count;
+        if (h_piece > h_cake)
+            h_piece = h_cake;
+        scr->slice_count += h_piece;
+        if (scr->slice_count == scr->h_dirty)
+            scr->slice_status = SGL_SLICE_STATUS_IDLE;
+        sgl_set_rect(&scr->slice, scr->offset_x, scr->offset_y,
+                     scr->offset_x + w_piece - 1, scr->offset_y + h_piece - 1);
+        scr->drawable = scr->slice;
+        break;
+    }
 }
 
 static void sgl_rotate_drawable_rect(sgl_screen_t *scr) {
@@ -58,16 +70,12 @@ static void sgl_rotate_drawable_rect(sgl_screen_t *scr) {
 }
 
 void sgl_handler(sgl_screen_t *scr) {
-    // sgl_buffer_slice(scr);
+    sgl_buffer_slice(scr);
     // sgl_rotate_drawable_rect(scr);
     scr->draw(scr);
-    scr->flush(scr->buffer, &scr->drawable);
-    ++scr->fcount;
-}
-
-void sgl_set_buffer(sgl_screen_t *scr, void *buffer, uint32_t buffer_size) {
-    scr->buffer = buffer;
-    scr->buffer_size = buffer_size;
+    scr->flush(scr->buffer, &scr->slice);
+    if (scr->slice_status == SGL_SLICE_STATUS_IDLE)
+        ++scr->frame_count;
 }
 
 void sgl_set_draw(sgl_screen_t *scr, void (*draw)(sgl_screen_t *scr)) {
@@ -79,10 +87,61 @@ void sgl_set_flush(sgl_screen_t *scr,
     scr->flush = flush;
 }
 
-void sgl_set_draw_pixel(sgl_screen_t *scr,
-                        void (*draw_pixel)(sgl_screen_t *scr, int32_t x,
-                                           int32_t y, uint32_t color)) {
+int sgl_set_draw_pixel(sgl_screen_t *scr,
+                       void (*draw_pixel)(sgl_screen_t *scr, int32_t x,
+                                          int32_t y, uint32_t color),
+                       uint32_t buffer_size, uint32_t pixel_size) {
+    if (buffer_size == 0 || pixel_size == 0)
+        return -1;
     scr->draw_pixel = draw_pixel;
+    scr->pixel_num = buffer_size / pixel_size;
+    scr->buffer_size = scr->pixel_num * pixel_size;
+    return 0;
+}
+
+static void sgl_set_color_format(sgl_screen_t *scr, uint32_t buffer_size,
+                                 sgl_color_format_t color_format) {
+    switch (color_format) {
+    case SGL_COLOR_FORMAT_MONO:
+        sgl_set_draw_pixel(scr, sgl_draw_pixel_mono, buffer_size, 1);
+        break;
+    case SGL_COLOR_FORMAT_RGB332:
+        sgl_set_draw_pixel(scr, sgl_draw_pixel_rgb332, buffer_size, 1);
+        break;
+    case SGL_COLOR_FORMAT_RGB565:
+        sgl_set_draw_pixel(scr, sgl_draw_pixel_rgb565, buffer_size, 2);
+        break;
+    case SGL_COLOR_FORMAT_RGB565SWAP:
+        sgl_set_draw_pixel(scr, sgl_draw_pixel_rgb565swap, buffer_size, 2);
+        break;
+    case SGL_COLOR_FORMAT_BGR565:
+        sgl_set_draw_pixel(scr, sgl_draw_pixel_bgr565, buffer_size, 2);
+        break;
+    case SGL_COLOR_FORMAT_RGB888:
+        sgl_set_draw_pixel(scr, sgl_draw_pixel_rgb888, buffer_size, 3);
+        break;
+    case SGL_COLOR_FORMAT_BGR888:
+        sgl_set_draw_pixel(scr, sgl_draw_pixel_bgr888, buffer_size, 3);
+        break;
+    case SGL_COLOR_FORMAT_XRGB8888:
+        sgl_set_draw_pixel(scr, sgl_draw_pixel_xrgb8888, buffer_size, 4);
+        break;
+    case SGL_COLOR_FORMAT_XBGR8888:
+        sgl_set_draw_pixel(scr, sgl_draw_pixel_xbgr8888, buffer_size, 4);
+        break;
+    case SGL_COLOR_FORMAT_ARGB8888:
+        sgl_set_draw_pixel(scr, sgl_draw_pixel_argb8888, buffer_size, 4);
+        break;
+    case SGL_COLOR_FORMAT_ABGR8888:
+        sgl_set_draw_pixel(scr, sgl_draw_pixel_abgr8888, buffer_size, 4);
+        break;
+    case SGL_COLOR_FORMAT_RGBA8888:
+        sgl_set_draw_pixel(scr, sgl_draw_pixel_rgba8888, buffer_size, 4);
+        break;
+    case SGL_COLOR_FORMAT_BGRA8888:
+        sgl_set_draw_pixel(scr, sgl_draw_pixel_bgra8888, buffer_size, 4);
+        break;
+    }
 }
 
 void sgl_set_drawable(sgl_screen_t *scr, int32_t left, int32_t top,
@@ -119,9 +178,9 @@ void sgl_set_screen_rotation(sgl_screen_t *scr, sgl_rotate_t rotate) {
     sgl_set_rect(&scr->drawable, 0, 0, scr->max_x, scr->max_y);
 }
 
-uint32_t sgl_get_fcount(sgl_screen_t *scr) { return scr->fcount; }
+uint32_t sgl_get_frame_count(sgl_screen_t *scr) { return scr->frame_count; }
 
-void sgl_reset_fcount(sgl_screen_t *scr) { scr->fcount = 0; }
+void sgl_reset_frame_count(sgl_screen_t *scr) { scr->frame_count = 0; }
 
 void sgl_clear_screen(sgl_screen_t *scr, uint8_t value) {
     memset(scr->buffer, value, scr->buffer_size);
